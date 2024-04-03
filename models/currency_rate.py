dce2c02b269849d043b23c20ed338cbecd3b5fc8
mariogustavoorrillo <gustavo.orrillo@gmail.com>
2024-04-03 11:48:48 +0000

    Added currency conversion-

--- models/currency_rate.py
+++ models/currency_rate.py
@@ -15,5 +15,12 @@ class ResCurrencyRate(models.Model):
                 price = product_tmpl.seller_ids[0].price
                 new_price = currency._convert(price,currency_id,company=self.env.user.company_id,date=date.today())
                 product_tmpl.standard_price = new_price
+        seller_ids = self.env['product.supplierinfo'].search([('currency_id','=',currency_id.id)])
+        for seller in seller_ids:
+            local_currency = seller.currency_id
+            currency = product_tmpl.currency_id
+            price = seller.price
+            new_price = local_currency._convert(price,currency_id,company=self.env.user.company_id,date=date.today())
+            product_tmpl.standard_price = new_price
         return res
 
